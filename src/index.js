04b0bdb642807602ff563f54ebe79cbbd6087b34
Peregrin <46428773+Peregrinn@users.noreply.github.com>
2021-12-23 15:59:12 -0600

    Update v0.1.1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron');
-const ejs = require('ejs-electron');
+const { app, shell, BrowserWindow, session } = require('electron');
+const ejs = require('ejs');
+const ejsEL = require('ejs-electron');
 const path = require('path');
 const fetch = require('node-fetch');
 const jq = require('jquery');
@@ -21,7 +22,22 @@ const createWindow = () => {
       nodeIntegration: true,
       contextIsolation: false
     }
-  })
+  });
+  const loginWindow = new BrowserWindow({
+    width: 350,
+    height: 450,
+    minHeight: 450,
+    minWidth: 350,
+    resizable: true,
+    icon: __dirname + '/views/public/favicon.ico',
+    autoHideMenuBar: true,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      nativeWindowOpen: false
+    }
+  });
   const mainWindow = new BrowserWindow({
     width: 575,
     height: 760,
@@ -39,19 +55,36 @@ const createWindow = () => {
 
   splashWindow.loadFile(path.join(__dirname, '/views/splash.ejs'));
   mainWindow.loadFile(path.join(__dirname, '/views/index.ejs'));
-
-  fetch(`https://www.eiffelware.net/api/apps/pimhash/0.1`, {
+  loginWindow.loadFile(path.join(__dirname, '/views/login.ejs'));
+  mainWindow.webContents.on("new-window", function(event, url) {
+    event.preventDefault();
+    shell.openExternal(url);
+  });
+  
+  let uss;
+  mainWindow.webContents.executeJavaScript('localStorage.getItem("session")').then(object => uss = object);
+  fetch(`https://www.eiffelware.net/api/apps/pimhash/0.1.1`, {
       method: 'get'
     }).then((r) => r.json()).then((b) => {
-      splashWindow.once('close', () => {
-        if (!b.auth) return app.quit()
-        if (b.update) return app.quit()
-        if (b.auth) { return mainWindow.show() }
-        else { app.quit() };
-      })
-    });
+        splashWindow.once('close', () => {
+          fetch(`https://www.eiffelware.net/api/v1/apps/pimhash/auth`, { method: 'post', headers: { session: uss } }).then((r) => r.json()).then((DD) => {
+            if (!b.auth || !b.loginAuth) return app.quit()
+            if (b.update) return app.quit()
+            if (b.auth && DD.OK == true) return mainWindow.show()
+            if (b.auth && DD.OK == false) return loginWindow.show()
+            app.quit()
+          });
+        });
+
+        loginWindow.once('close', () => {
+          fetch(`https://www.eiffelware.net/api/v1/apps/pimhash/auth`, { method: 'post', headers: { session: uss } }).then((r) => r.json()).then((res) => {
+            if (res.OK == true) return mainWindow.show()
+            app.quit();
+        }).catch(err => app.quit())});
+      }).catch(err => app.quit());
 
   mainWindow.setMenu(null);
+  loginWindow.setMenu(null);
   splashWindow.setMenu(null);
   //mainWindow.webContents.openDevTools(); 
 };
--- src/views/public/js/splash.js
+++ src/views/public/js/splash.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 var progress = document.getElementById('progressBar');
 var a = document.getElementById('text');
 
-fetch(`https://www.eiffelware.net/api/apps/pimhash/0.1`, {
+fetch(`https://www.eiffelware.net/api/apps/pimhash/0.1.1`, {
     method: 'get'
 }).then((r) => r.json()).then((b) => {
     console.log(b);
